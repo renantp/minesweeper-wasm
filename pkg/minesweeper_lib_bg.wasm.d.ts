e80f1690aae3831ab4c5c986df2a01b6950106ad
renantp <phuannguyenhoang@gmail.com>
2023-05-22 13:27:57 +0700

    Add build to run

---
+++ pkg/minesweeper_lib_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function greet(a: number, b: number): void;
+export function getState(a: number): void;
+export function openFields(a: number, b: number): void;
+export function toggleFlag(a: number, b: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
